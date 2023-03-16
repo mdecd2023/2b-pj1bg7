7c4f21ad9feab69a3a02846c4fc454dd6264e9ab
YHY30678 <yhywtf30678@gmail.com>
2023-03-16 15:08:15 +0800

    upload w3 title

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 組員:詹耀賢、 \n 倉儲: https://github.com/mdecd2023/2b-pj1bg7', 'tags': '', 'url': 'About.html'}]};
-=======
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 倉儲: https://github.com/mdecd2023/2b-pj1bg7 \n 組員:41023240、41023241 \n', 'tags': '', 'url': 'About.html'}]};
->>>>>>> b3cad05dd19c68a6e8fae221704ca4a492527d38
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 倉儲 \n 組員:詹耀賢、葉桓亞 \n', 'tags': '', 'url': 'About.html'}, {'title': 'w3', 'text': '', 'tags': '', 'url': 'w3.html'}, {'title': 'bubbleRob', 'text': '\n \n', 'tags': '', 'url': 'bubbleRob.html'}]};
