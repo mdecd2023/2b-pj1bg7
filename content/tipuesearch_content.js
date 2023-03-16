b3cad05dd19c68a6e8fae221704ca4a492527d38
YHY30678 <yhywtf30678@gmail.com>
2023-03-16 14:39:17 +0800

    change password

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 倉儲: https://github.com/mdecd2023/2b-pj1bg7 \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 倉儲: https://github.com/mdecd2023/2b-pj1bg7 \n 組員:41023240、41023241 \n', 'tags': '', 'url': 'About.html'}]};
