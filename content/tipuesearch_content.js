52e6e621e99068b438097c40ef05a096f4a83c81
Yao-Hsien41023241 <41023241@gm.nfu.edu.tw>
2023-03-20 20:17:54 +0800

    del

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 123 HEAD \n 倉儲: https://github.com/mdecd2023/2b-pj1bg7 \n 組員:41023240、41023241 \n \n \xa0b3cad05dd19c68a6e8fae221704ca4a492527d38', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 倉儲: https://github.com/mdecd2023/2b-pj1bg7 \n 組員:41023240、41023241 \n', 'tags': '', 'url': 'About.html'}]};
